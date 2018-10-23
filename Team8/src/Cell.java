1e5ef9d8259db02e6161d409142b3747f51ee154
Brady Opsahl <bcopsahl@mtu.edu>
2018-10-23 14:58:38 -0400

    Implemented basic cells and list
    Basic implementation of cells and a list to contain them. Has a populate() method to fill spaces with a 20% chance of a cell being alive, and has a print() method that prints out the list as a square. Currently only supports 100 cells.

---
+++ Team8/src/Cell.java
@@ -0,0 +1,17 @@
+
+public class Cell {
+
+	private boolean alive;
+	
+	public Cell( boolean isAlive ) {
+		alive = isAlive;	
+	}
+	
+	public boolean getAlive() {
+		return alive;
+	}
+	
+	public void setAlive( boolean isAlive ) {
+		alive = isAlive;
+	}
+}
---
+++ Team8/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+	// list of all cells
+	private static ArrayList<Cell> cells;
+	
+	// populates empty list with 100 cells that have a 20% chance of being alive
+	public void populate() {
+		
+		Random rand = new Random(System.nanoTime());
+		
+		for( int i = 0; i < 100; i++ ) {
+			if( rand.nextInt(5) <= 0 ) {
+				cells.add( new Cell(true) );
+			} else {
+				cells.add( new Cell(false) );
+			}
+		}
+	}
+	
+	public void print() {
+		
+		int lineCount = 0;
+		
+		for( Cell c: cells ) {
+			if( lineCount >= 10 ) {
+				System.out.println();
+				lineCount = 0;
+			}
+			
+			lineCount++;
+			
+			if( c.getAlive() ) {
+				System.out.print("O");
+			} else {
+				System.out.print("X");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		cells = new ArrayList<Cell>();
+		
+		main.populate();
+		
+		main.print();
+
+	}
+
+}
