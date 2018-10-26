805d7cd18b0a2f6250733d07c81676b1fe86948b
FoamCactus <cahill@mtu.edu>
2018-10-26 16:32:31 -0500

    added some generation logic

--- Team8/src/main/java/Main.java
+++ Team8/src/main/java/Main.java
@@ -25,39 +25,61 @@ public class Main {
 			}
 		}
 	}
+	// checks a three wide row if things are alive 
+	public int leftRight(int index){
+		int count = 0;
+		if((index % dimension) <= (dimension - 1)) {
+			if(index>0 && cells.get(index-1).getAlive()){
+				count++;
+			}
+		}
+		if((index % dimension) >= 0 ){
+			if(index+1 < size && cells.get(index+1).getAlive()){
+				count++;
+			}
+		}
+		if( index < size && cells.get(index).getAlive()){
+			count++;
+		}
+		return count;
+	} 
 	
 	//takes a cell index position and outputs the number of alive cells nearby
 	public int detectNearby(int index) {
 		int nearbycount = 0;
 		
-		//tests if above cell
+		//top three cells
 		if(index > dimension) {
-			if(cells.get(index-dimension).getAlive()) {
-				nearbycount++;
-			}
+			nearbycount += leftRight(index-dimension);
 		}
-		//tests if left cell
-		if(index > 0) {
-			if(cells.get(index-1).getAlive()) {
-				nearbycount++;
-			}
+		//middle three cells
+		nearbycount += leftRight(index);
+		if(cells.get(index).getAlive()){
+			nearbycount--;
 		}
-		//tests if right cell
-		if(index < size - 1) {
-			if(cells.get(index+1).getAlive()) {
-				nearbycount++;
-			}
+		//Bottom three cells
+		if(index < size && (index + dimension) < size) {
+			nearbycount += leftRight(index + dimension);
 		}
-		//Tests bottom cell
-		if(index < size) {
-			if(cells.get(index+dimension).getAlive()) {
-				nearbycount++;
-			}
-		}		
-		
 		return nearbycount;
 	}
-	
+	//based on the number of alive cells nearby returns if the cell is alive
+	public boolean aliveOrDead(int index) {
+		int count = detectNearby(index);
+		if( cells.get(index).getAlive()){
+			if ( count == 2 || count == 3) {
+				return true;
+			}
+			return false;
+		} else {
+			if(count == 3){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//print function	
 	public void print() {
 		
 		int lineCount = 0;
@@ -78,6 +100,16 @@ public class Main {
 		}
 		System.out.println();
 	}
+
+	//makes a full new generation
+	public void generation(){
+		ArrayList<Cell> nextgen = new ArrayList<Cell>();
+
+		for(int i =0; i <size;i++) {
+			nextgen.add(i,new Cell(aliveOrDead(i)));
+		}
+		cells = nextgen;
+	}
 	
 	public static void main(String[] args) {
 		
@@ -85,8 +117,11 @@ public class Main {
 		main.cells = new ArrayList<Cell>();
 		
 		main.populate( );
-		
-		main.print();
+		for( int i = 0; i <10; i++){
+			main.print();
+			System.out.println();
+			main.generation();
+		}			
 
 		System.out.println("Nearby to cell 0: " + main.detectNearby(0));
 	}
