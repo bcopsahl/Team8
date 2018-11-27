8cd18dc7bec15626f4083f62d766440cb542df44
FoamCactus <cahill@mtu.edu>
2018-11-26 20:07:58 -0500

    little changes, might help cross platform

--- Team8/src/main/java/BoardFactory.java
+++ Team8/src/main/java/BoardFactory.java
@@ -12,6 +12,7 @@ public class BoardFactory {
     public static Board  getBoard() {
         int x,y;
         x = y = 10;
+        Board result;
         Scanner reader = new Scanner(System.in);
         System.out.println("Type of board: [infinite,bounded,wrap-around]");
         String boardType = reader.nextLine();
@@ -23,10 +24,11 @@ public class BoardFactory {
             System.out.println("Enter the y dimension: ");
             y = reader.nextInt();
         } else {
-            y = getConsoleDimension("lines");
-            x = getConsoleDimension("cols")/2;
+            if (!boardType.equals("infinite")){ 
+                y = getConsoleDimension("lines")-1;
+                x = (getConsoleDimension("cols")-1)/2;
+            }
         }
-        Board result;
         switch (boardType) {
             case "infinite": result = new infiniteBoard(x,y);
                             break;
@@ -41,30 +43,35 @@ public class BoardFactory {
     
     }
     private static int getConsoleDimension(String dimension){
-        ProcessBuilder pb = new ProcessBuilder("tput",dimension);
-        InputStreamReader i;
-        int j;
-        char c;
-        Character[] d;
-        List<Character> l = new ArrayList<Character>();
-        try{
-            Process process  = pb.start();
-            i = new InputStreamReader(process.getInputStream());
-            while((j = i.read())!= -1) {
-                l.add(new  Character((char)j));
+        if( System.getProperty("os.name").indexOf("nux")>=0){
+            ProcessBuilder pb = new ProcessBuilder("tput",dimension);
+            InputStreamReader i;
+            int j;
+            char c;
+            Character[] d;
+            List<Character> l = new ArrayList<Character>();
+            try{
+                Process process  = pb.start();
+                // this next line is confusing, its input as in this process input
+                i = new InputStreamReader(process.getInputStream());
+                while((j = i.read())!= -1) {
+                    l.add(new  Character((char)j));
+                }
+                process.waitFor();
+            } catch( Exception e ){
+                e.printStackTrace();
             }
-            process.waitFor();
-        } catch( Exception e ){
-            e.printStackTrace();
+            char[] e = new char[l.size()];
+            for(int k =0; k < l.size(); k++){
+                e[k] = l.get(k).charValue();
+            }
+            //this may not be necessary, however somehow parsing charachter outputs is hard
+            String result = new String(e).replaceAll("\\r|\\n|\"","");
+            int r =  Integer.valueOf(result);
+            return r;
+        } else {
+            return 10;
         }
-        char[] e = new char[l.size()];
-        for(int k =0; k < l.size(); k++){
-            e[k] = l.get(k).charValue();
-        }
-        String result = new String(e).replaceAll("\\r|\\n|\"","");
-
-        int r =  Integer.valueOf(result);
-        return r-1;
     }
 
 }
